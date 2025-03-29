b6b4a20499c535e81a1328387268111d323b7f95
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-29 07:57:15 -0600

    TypeScript 03 - Tipos de datos básicos

--- public/main.js
+++ public/main.js
@@ -1,5 +1,29 @@
 "use strict";
-console.log("Hello world pana");
-console.log("Hello world");
-console.log("Hello world 2");
-console.log("Hello world 3");
+//Tipos de datos
+let movie = "La casa de papel";
+let duration = 200;
+let hasOscars = true;
+movie = "La casa de papel 2";
+//movie = 123;
+duration = 200;
+//duration = "200";
+hasOscars = true;
+//hasOscars = 123;
+let myObject = {
+    name: "Ulises",
+    age: 30,
+    movie: "La casa de papel",
+    duration: 200,
+    hasOscars: true,
+};
+myObject = {
+    name: "Jesus",
+};
+//myObject=[];
+let myArray = [1, 2, 3, 4, 5];
+myArray = [10, 20, 30, 40, 50];
+//myArray=['a','b','c','d','e'];
+//evitar usar any no tiene sentido si es typescript
+let data = 999;
+data = 'hola';
+console.log(movie, duration, hasOscars);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,38 @@
-console.log("Hello world pana");
-console.log("Hello world");
-console.log("Hello world 2");
-console.log("Hello world 3");
+//Tipos de datos
+
+let movie: string = "La casa de papel";
+let duration: number = 200;
+let hasOscars: boolean = true;
+
+movie = "La casa de papel 2";
+//movie = 123;
+
+duration = 200;
+//duration = "200";
+
+hasOscars = true;
+//hasOscars = 123;
+
+let myObject : Object = {
+    name: "Ulises",
+    age: 30,
+    movie: "La casa de papel",
+    duration: 200,
+    hasOscars: true,
+};
+
+myObject = {
+    name: "Jesus",
+}
+
+//myObject=[];
+
+let myArray:number[]=[1,2,3,4,5];
+myArray=[10,20,30,40,50];
+//myArray=['a','b','c','d','e'];
+
+//evitar usar any no tiene sentido si es typescript
+let data:any= 999;
+data='hola';
+
+console.log(movie,duration,hasOscars);
