9f1a3ce21e65a4ff6dd3f7e6fb45529afd305177
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-29 09:42:42 -0600

    TypeScript 05 - Funciones

--- public/main.js
+++ public/main.js
@@ -1,24 +1,14 @@
 "use strict";
-// TypeScript 04 - Tipos de datos avanzados
-//union
-let myid = 1;
-myid = "hola";
-let userId = "hola2";
-let myarra = [1, "hola", true];
-myarra = [1, 2, 7, 6, 5, "hola", true];
-let loading = "loading";
-let error = "error";
-let state = "loading";
-const esdla = {
-    title: "El secreto de la muerte",
-    duration: 120,
-    hasOscar: true,
-};
-const book1 = {
-    title: "El secreto de la muerte",
-    pages: 120,
-    duration: 120,
-    hasOscar: true,
-};
-//let numString: numString = 1;
-console.log(book1, esdla);
+//Functions
+function sum(a, b) {
+    return a + b;
+}
+let result = sum(1, 2);
+console.log(result);
+const toUpper = (str) => str.toUpperCase();
+const str = toUpper("hello");
+console.log(str);
+function sayHello() {
+    console.log("Hello!");
+}
+sayHello();
--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,20 @@
-// TypeScript 04 - Tipos de datos avanzados
+//Functions
 
-//union
-let myid: number | string = 1;
-myid = "hola";
+function sum(a: number, b: number) {
+    return a + b;
+}
 
-type id = number | string;
+let result = sum(1, 2);
+console.log(result);
 
-let userId: id = "hola2";
+const toUpper = (str:string) => str.toUpperCase();
+const str = toUpper("hello");
+console.log(str);
 
-let myarra:(number | string | boolean)[] = [1, "hola", true];
+function sayHello() {
+    console.log("Hello!");
 
-myarra = [1,2,7,6,5, "hola", true];
+}
 
-//Litera;
-type stateLoading = "loading";
-type stateError = "error";
-
-let loading: stateLoading = "loading";
-let error: stateError = "error";
-
-type state = stateLoading | stateError;
-
-let state: state = "loading";
-
-type movie = {
-  title: string;
-  duration: number;
-  hasOscar: boolean;
-};
-
-const esdla: movie = {
-  title: "El secreto de la muerte",
-  duration: 120,
-  hasOscar: true,
-};
-
-//Intersección
-type book = {
-  title: string;
-  pages: number;
-};
-
-type bookAdaptation = movie & book;
-
-const book1: bookAdaptation = {
-  title: "El secreto de la muerte",
-  pages: 120,
-  duration: 120,
-  hasOscar: true,
-};
-
-type numString = number & string;
-//let numString: numString = 1;
-
-console.log(book1,esdla);
+sayHello();
+ 
